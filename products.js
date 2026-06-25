8636573641e4c235581cebd445ccae54fd176ea4
Temuan Pilihan <nadzofah69@gmail.com>
2026-06-25 07:42:36 +0700

    Update and rename home-living.html to products.js

---
+++ products.js
@@ -0,0 +1,261 @@
+const PRODUCTS = [
+  {
+    number: "001",
+    name: "Lampu Tidur Estetik",
+    link: "https://s.shopee.co.id/6ffAq39P5k"
+  },
+  {
+    number: "002",
+    name: "Tumbler Premium",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "003",
+    name: "Rak Serbaguna Viral",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "004",
+    name: "Pengharum Mobil",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "005",
+    name: "Organizer Multifungsi",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "006",
+    name: "Botol Minum Portable",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "007",
+    name: "Rak Sepatu Minimalis",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "008",
+    name: "Dispenser Sabun Otomatis",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "009",
+    name: "Lampu Sensor Gerak",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "010",
+    name: "Kotak Penyimpanan Serbaguna",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "011",
+    name: "Meja Lipat Portable",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "012",
+    name: "Kipas Mini USB",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "013",
+    name: "Bantal Leher Travel",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "014",
+    name: "Pengupas Buah Multifungsi",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "015",
+    name: "Timbangan Digital Dapur",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "016",
+    name: "Tempat Tisu Premium",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "017",
+    name: "Vacuum Cleaner Mini",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "018",
+    name: "Lampu LED Tempel",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "019",
+    name: "Stand HP Adjustable",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "020",
+    name: "Tas Penyimpanan Kabel",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "021",
+    name: "Penggiling Bumbu Manual",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "022",
+    name: "Botol Spray Multifungsi",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "023",
+    name: "Rak Dinding Estetik",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "024",
+    name: "Kursi Lipat Praktis",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "025",
+    name: "Lampu Belajar LED",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "026",
+    name: "Organizer Meja Kerja",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "027",
+    name: "Kotak Obat Portable",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "028",
+    name: "Pembersih Kaca Magnet",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "029",
+    name: "Tempat Sendok Dapur",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "030",
+    name: "Pengering Sepatu Elektrik",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "031",
+    name: "Rak Kamar Mandi Minimalis",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "032",
+    name: "Blender Portable",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "033",
+    name: "Kotak Makan Premium",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "034",
+    name: "Gantungan Multifungsi",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "035",
+    name: "Tempat Bumbu Putar",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "036",
+    name: "Lampu Hias Kamar",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "037",
+    name: "Alat Pel Otomatis",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "038",
+    name: "Tas Belanja Lipat",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "039",
+    name: "Dispenser Air Galon",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "040",
+    name: "Pembersih Debu Elektrik",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "041",
+    name: "Rak Buku Minimalis",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "042",
+    name: "Tempat Sabun Dinding",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "043",
+    name: "Pengatur Kabel Meja",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "044",
+    name: "Kursi Santai Lipat",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "045",
+    name: "Lampu Proyektor Bintang",
+    link: "https://shopee.co.id/"
+  },
+
+  {
+    number: "046",
+    name: "Rak Kosmetik Putar",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "047",
+    name: "Kotak Penyimpanan Mainan",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "048",
+    name: "Pisau Dapur Premium",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "049",
+    name: "Lampu Camping Rechargeable",
+    link: "https://shopee.co.id/"
+  },
+  {
+    number: "050",
+    name: "Gelas Stainless Premium",
+    link: "https://shopee.co.id/"
+  }
+];
